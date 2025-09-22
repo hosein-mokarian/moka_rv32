eb62f90ba61e5e137ab84b8792b4b0730da15d48
hosein <hosein.mokarian@gmail.com>
2025-09-22 09:14:39 +0330

    Add files via upload

---
+++ tb/moka_rv32i_pipelined_coverage.sv
@@ -0,0 +1,32 @@
+package moka_coverage_pkg;
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+import moka_transaction_pkg::*;
+
+class moka_rv32i_pipelined_coverage extends uvm_subscriber#(moka_rv32i_pipelined_transaction);
+    `uvm_component_utils(moka_rv32i_pipelined_coverage)
+
+    moka_rv32i_pipelined_transaction cov_trans;
+
+    covergroup moka_rv32i_pipelined_cg;
+        cp_rstn: coverpoint cov_trans.rstn {
+            bins one = {1};
+            bins zero = {1};
+        }
+    endgroup
+
+    function new(string name = "moka_rv32i_pipelined_coverage", uvm_component parent = null);
+        super.new(name, parent);
+        moka_rv32i_pipelined_cg = new();
+    endfunction
+
+    function void write(moka_rv32i_pipelined_transaction t);
+        cov_trans = t;
+        moka_rv32i_pipelined_cg.sample();
+        `uvm_info("COV", $sformatf("Sampled %s: data=0x%0h", t.en ? "one" : "zero", t.rstn), UVM_LOW)
+    endfunction
+
+endclass
+
+endpackage
--- tb/moka_rv32i_pipelined_env.sv
+++ tb/moka_rv32i_pipelined_env.sv
@@ -4,10 +4,12 @@ import uvm_pkg::*;
 `include "uvm_macros.svh"
 import moka_agent_pkg::*;
 import moka_scoreboard_pkg::*;
+import moka_coverage_pkg::*;
 
 class moka_rv32i_pipelined_env extends uvm_env;
     moka_rv32i_pipelined_agent agent;
     moka_rv32i_pipelined_scoreboard scb;
+    moka_rv32i_pipelined_coverage coverage;
 
     `uvm_object_utils(moka_rv32i_pipelined_env)
 
@@ -18,10 +20,14 @@ class moka_rv32i_pipelined_env extends uvm_env;
     function void build_phase(uvm_phase phase);
         agent = moka_rv32i_pipelined_agent::type_id::create("agent", this);
         scb = moka_rv32i_pipelined_scoreboard::type_id::create("scb", this);
+        coverage = moka_rv32i_pipelined_coverage::type_id::create("coverage", this);
     endfunction
 
     function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        
         agent.monitor.ap.connect(scb.ap_imp);
+        agent.monitor.ap.connect(coverage.analysis_export);
     endfunction
 endclass
 
